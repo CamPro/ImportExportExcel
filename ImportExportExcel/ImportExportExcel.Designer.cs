1afc830c59927b84b05a73f76cebee9f73518709
DESKTOP-F27LIG1\Adminc <toisekhongcamchiu@gmail.com>
2023-10-22 11:16:05 +0700

    add file

---
+++ ImportExportExcel/ImportExportExcel.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ImportExportExcel
+{
+    partial class ImportExportExcel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonExportDataTableToExcel = new System.Windows.Forms.Button();
+            this.buttonExportDataGridViewToExcel = new System.Windows.Forms.Button();
+            this.buttonExportDataBaseToExcel = new System.Windows.Forms.Button();
+            this.buttonAsposeImportExcel = new System.Windows.Forms.Button();
+            this.buttonAsposeExportExcel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(291, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(534, 388);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // buttonExportDataTableToExcel
+            // 
+            this.buttonExportDataTableToExcel.Location = new System.Drawing.Point(13, 13);
+            this.buttonExportDataTableToExcel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonExportDataTableToExcel.Name = "buttonExportDataTableToExcel";
+            this.buttonExportDataTableToExcel.Size = new System.Drawing.Size(270, 64);
+            this.buttonExportDataTableToExcel.TabIndex = 1;
+            this.buttonExportDataTableToExcel.Text = "Export DataTable To Excel";
+            this.buttonExportDataTableToExcel.UseVisualStyleBackColor = true;
+            this.buttonExportDataTableToExcel.Click += new System.EventHandler(this.buttonExportDataTableToExcel_Click);
+            // 
+            // buttonExportDataGridViewToExcel
+            // 
+            this.buttonExportDataGridViewToExcel.Location = new System.Drawing.Point(13, 85);
+            this.buttonExportDataGridViewToExcel.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonExportDataGridViewToExcel.Name = "buttonExportDataGridViewToExcel";
+            this.buttonExportDataGridViewToExcel.Size = new System.Drawing.Size(270, 64);
+            this.buttonExportDataGridViewToExcel.TabIndex = 2;
+            this.buttonExportDataGridViewToExcel.Text = "Export DataGridView To Excel";
+            this.buttonExportDataGridViewToExcel.UseVisualStyleBackColor = true;
+            this.buttonExportDataGridViewToExcel.Click += new System.EventHandler(this.buttonExportDataGridViewToExcel_Click);
+            // 
+            // buttonExportDataBaseToExcel
+            // 
+            this.buttonExportDataBaseToExcel.Location = new System.Drawing.Point(13, 157);
+            this.buttonExportDataBaseToExcel.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonExportDataBaseToExcel.Name = "buttonExportDataBaseToExcel";
+            this.buttonExportDataBaseToExcel.Size = new System.Drawing.Size(270, 64);
+            this.buttonExportDataBaseToExcel.TabIndex = 3;
+            this.buttonExportDataBaseToExcel.Text = "Export DataBase To Excel";
+            this.buttonExportDataBaseToExcel.UseVisualStyleBackColor = true;
+            this.buttonExportDataBaseToExcel.Click += new System.EventHandler(this.buttonExportDataBaseToExcel_Click);
+            // 
+            // buttonAsposeImportExcel
+            // 
+            this.buttonAsposeImportExcel.Location = new System.Drawing.Point(13, 229);
+            this.buttonAsposeImportExcel.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonAsposeImportExcel.Name = "buttonAsposeImportExcel";
+            this.buttonAsposeImportExcel.Size = new System.Drawing.Size(208, 64);
+            this.buttonAsposeImportExcel.TabIndex = 4;
+            this.buttonAsposeImportExcel.Text = "Aspose Import Excel";
+            this.buttonAsposeImportExcel.UseVisualStyleBackColor = true;
+            this.buttonAsposeImportExcel.Click += new System.EventHandler(this.buttonAsposeImportExcel_Click);
+            // 
+            // buttonAsposeExportExcel
+            // 
+            this.buttonAsposeExportExcel.Location = new System.Drawing.Point(13, 301);
+            this.buttonAsposeExportExcel.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonAsposeExportExcel.Name = "buttonAsposeExportExcel";
+            this.buttonAsposeExportExcel.Size = new System.Drawing.Size(208, 64);
+            this.buttonAsposeExportExcel.TabIndex = 5;
+            this.buttonAsposeExportExcel.Text = "Aspose Export Excel";
+            this.buttonAsposeExportExcel.UseVisualStyleBackColor = true;
+            this.buttonAsposeExportExcel.Click += new System.EventHandler(this.buttonAsposeExportExcel_Click);
+            // 
+            // ImportExportExcel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 410);
+            this.Controls.Add(this.buttonAsposeExportExcel);
+            this.Controls.Add(this.buttonAsposeImportExcel);
+            this.Controls.Add(this.buttonExportDataBaseToExcel);
+            this.Controls.Add(this.buttonExportDataGridViewToExcel);
+            this.Controls.Add(this.buttonExportDataTableToExcel);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "ImportExportExcel";
+            this.Text = "ImportExportExcel";
+            this.Load += new System.EventHandler(this.ImportExportExcel_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonExportDataTableToExcel;
+        private System.Windows.Forms.Button buttonExportDataGridViewToExcel;
+        private System.Windows.Forms.Button buttonExportDataBaseToExcel;
+        private System.Windows.Forms.Button buttonAsposeImportExcel;
+        private System.Windows.Forms.Button buttonAsposeExportExcel;
+    }
+}
+
---
+++ ImportExportExcel/ImportExportExcel.cs
@@ -0,0 +1,214 @@
+﻿using Aspose.Cells;
+using Syncfusion.XlsIO;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImportExportExcel
+{
+    public partial class ImportExportExcel : Form
+    {
+        public ImportExportExcel()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void ImportExportExcel_Load(object sender, EventArgs e)
+        {
+            #region Loading the data to Data Grid
+            DataSet customersDataSet = new DataSet();
+            //Get the path of the input file
+            string inputXmlPath = Path.GetFullPath(@"../../Data/Employees.xml");
+            customersDataSet.ReadXml(inputXmlPath);
+            DataTable dataTable = new DataTable();
+            //Copy the structure and data of the table
+            dataTable = customersDataSet.Tables[1].Copy();
+            //Removing unwanted columns
+            dataTable.Columns.RemoveAt(0);
+            dataTable.Columns.RemoveAt(10);
+            this.dataGridView1.DataSource = dataTable;
+
+
+            dataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.White;
+            dataGridView1.RowsDefaultCellStyle.BackColor = Color.LightBlue;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Tahoma", 9F, ((System.Drawing.FontStyle)(System.Drawing.FontStyle.Bold)));
+            dataGridView1.ForeColor = Color.Black;
+            dataGridView1.BorderStyle = BorderStyle.None;
+
+            #endregion
+
+        }
+
+        private void ExportDataTableToExcel()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Excel2016;
+
+                //Create a new workbook
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                //Create a dataset from XML file
+                DataSet customersDataSet = new DataSet();
+                customersDataSet.ReadXml(Path.GetFullPath(@"../../Data/Employees.xml"));
+
+                //Create datatable from the dataset
+                DataTable dataTable = new DataTable();
+                dataTable = customersDataSet.Tables[1];
+
+                //Import data from the data table with column header, at first row and first column, and by its column type.
+                sheet.ImportDataTable(dataTable, true, 1, 1, true);
+
+                //Creating Excel table or list object and apply style to the table
+                IListObject table = sheet.ListObjects.Create("Employee_PersonalDetails", sheet.UsedRange);
+
+                table.BuiltInTableStyle = TableBuiltInStyles.TableStyleMedium14;
+
+                //Autofit the columns
+                sheet.UsedRange.AutofitColumns();
+
+                //Save the file in the given path
+                Stream excelStream = File.Create(Path.GetFullPath(@"Output.xlsx"));
+                workbook.SaveAs(excelStream);
+                excelStream.Dispose();
+                Process.Start(Path.GetFullPath(@"Output.xlsx"));
+            }
+        }
+
+        private void ExportDataGridViewToExcel()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+
+                //Create a workbook with single worksheet
+                IWorkbook workbook = application.Workbooks.Create(1);
+
+                IWorksheet worksheet = workbook.Worksheets[0];
+
+                //Import from DataGridView to worksheet
+                worksheet.ImportDataGridView(dataGridView1, 1, 1, true, true);
+
+                worksheet.UsedRange.AutofitColumns();
+                workbook.SaveAs("Output.xlsx");
+                Process.Start("Output.xlsx");
+            }
+        }
+
+        private void ExportDataBaseToExcel()
+        {
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Excel2016;
+
+                //Create a new workbook
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+                if (sheet.ListObjects.Count == 0)
+                {
+                    //Estabilishing the connection in the worksheet
+                    string dBPath = Path.GetFullPath(@"../../Data/Employee.mdb");
+                    string ConnectionString = "OLEDB;Provider=Microsoft.JET.OLEDB.4.0;Password=\"\";User ID=Admin;Data Source=" + dBPath;
+                    string query = "SELECT EmployeeID,FirstName,LastName,Title,HireDate,Extension,ReportsTo FROM [Employees]";
+                    IConnection Connection = workbook.Connections.Add("Connection1", "Sample connection with MsAccess", ConnectionString, query, ExcelCommandType.Sql);
+                    sheet.ListObjects.AddEx(ExcelListObjectSourceType.SrcQuery, Connection, sheet.Range["A1"]);
+                }
+
+                //Refresh Excel table to get updated values from database
+                sheet.ListObjects[0].Refresh();
+
+                sheet.UsedRange.AutofitColumns();
+
+                //Save the file in the given path
+                Stream excelStream = File.Create(Path.GetFullPath(@"Output.xlsx"));
+                workbook.SaveAs(excelStream);
+                excelStream.Dispose();
+                Process.Start(Path.GetFullPath(@"Output.xlsx"));
+            }
+        }
+
+        private void buttonExportDataTableToExcel_Click(object sender, EventArgs e)
+        {
+            ExportDataTableToExcel();
+        }
+
+        private void buttonExportDataGridViewToExcel_Click(object sender, EventArgs e)
+        {
+            ExportDataGridViewToExcel();
+        }
+
+        private void buttonExportDataBaseToExcel_Click(object sender, EventArgs e)
+        {
+            ExportDataBaseToExcel();
+        }
+
+        OpenFileDialog ofdExcel = new OpenFileDialog();
+        SaveFileDialog sfdExcel = new SaveFileDialog();
+
+        private void AsposeImportExcel()
+        {
+            ofdExcel.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            DialogResult fileExcel = ofdExcel.ShowDialog();
+
+            if (fileExcel == DialogResult.OK)
+            {
+                string pathFileExcel = ofdExcel.FileName;
+                //load file excel
+                Workbook workbook = new Workbook(pathFileExcel);
+                //chon sheet dau tien
+                Worksheet worksheet = workbook.Worksheets[0];
+                //lấy vung du lieu
+                Range range = worksheet.Cells.MaxDisplayRange;
+                //day du lieu vao datatable, ý nghĩa tham số (từ dòng, từ cột, tổng số dòng lấy, tổng số cột lấy, tự đặt tên cột theo dòng đầu tiên)
+                //link tham khảo: https://docs.aspose.com/display/cellsnet/Export+Data+from+Worksheet
+                DataTable dataTable = worksheet.Cells.ExportDataTable(0, 0, range.RowCount, range.ColumnCount, true);
+                dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void AsposeExportExcel()
+        {
+            string template = AppDomain.CurrentDomain.BaseDirectory + @"..\..\Data\" + "TemplateExport2.xlsx";
+            template = Path.GetFullPath(template);
+
+            WorkbookDesigner wbDesigner = new WorkbookDesigner();
+            //Lấy template
+            wbDesigner.Workbook = new Workbook(template);
+            Worksheet ws = wbDesigner.Workbook.Worksheets[0];
+            //fill du lieu don le 
+            //cấu hình cell co format: &=$<<ten_tu_dat>>
+            wbDesigner.SetDataSource("NGAY_XUAT", DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+            //fill table
+            //cấu hình cột có dạng: &=<<ten_data_source_tu_dat>>.<<ten_cot_tuong_ung_trong_data_source>>
+            wbDesigner.SetDataSource("DTS", (dataGridView1.DataSource as DataTable).DefaultView);
+            //fill du lieu don le
+            wbDesigner.SetDataSource("TOTAL_ROWS", (dataGridView1.DataSource as DataTable).Rows.Count);
+            wbDesigner.Process();
+
+            sfdExcel.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            sfdExcel.FileName = "Export";
+            if (sfdExcel.ShowDialog() == DialogResult.OK)
+            {
+                wbDesigner.Workbook.Save(sfdExcel.FileName);
+            }
+        }
+
+        private void buttonAsposeImportExcel_Click(object sender, EventArgs e)
+        {
+            AsposeImportExcel();
+        }
+
+        private void buttonAsposeExportExcel_Click(object sender, EventArgs e)
+        {
+            AsposeExportExcel();
+        }
+    }
+}
